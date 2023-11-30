cd96d0ed5202ab12d94d21bcc944c5ea770b57f3
Ander Aramendi <aramendiander@gmail.com>
2023-11-30 01:24:01 +0100

    add info to more info cards

--- src/components/moreInfoCard.jsx
+++ src/components/moreInfoCard.jsx
@@ -1,20 +1,39 @@
+import moment from "moment"
+
 const MoreInfoCard = ({ country, index, onClick }) => {
 
+  const getCurrentTime = (timezone) => {
+    let offset = parseInt(timezone.replace('UTC', ''));
+    return moment.utc().add(offset, 'hours').format('HH:mm:ss');
 
-  console.log(country.languages)
+  }
 
   return (
     <div className="moreinfo">
       <h2>{country.name.common}</h2>
       <p>Official name: {country.name.official}</p>
+      <img src={country.flags.svg} alt="" />
       <p>Continent: {country.region}</p>
+      <p>Subregion: {country.subregion}</p>
       <p className="languages"> Languages:
         {Object.entries(country.languages).map(([key, value]) => (
           <span className="singlelanguage" key={key}>{' ' + value + ' '}</span>
         ))}
       </p>
       <p>Population: {country.population}</p>
-      <img src={country.flags.svg} alt="" />
+      <p className="timezones"> Timezones:
+        {Object.entries(country.timezones).map(([key, value]) => (
+          <span className="singleTimezone" key={key}>{' ' + getCurrentTime(value) + ', '}</span>
+        ))}
+        </p>
+        <p className="currencies"> Currencies:
+    {Object.entries(country.currencies).map(([currency, value]) => (
+
+            <span key={value.name}>{' '+value.name}</span>
+
+    ))}
+</p>
+
 
     </div>
   )
